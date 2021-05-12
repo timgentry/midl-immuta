b7c6962221d1c3d3d2cd2850f0ff1714b195b060
Tim Gentry <timgentry@users.noreply.github.com>
2021-05-12 08:06:11 +0100

    Scaffolded rubygem

---
+++ lib/midl/immuta.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "immuta/version"
+
+module Midl
+  module Immuta
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/midl/immuta/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Midl
+  module Immuta
+    VERSION = "0.1.0"
+  end
+end
